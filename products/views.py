f07ebab7a282ff5a1e5aa8506ea85c4c915b0c69
petrugio <petruchelban@gmail.com>
2022-12-02 15:52:17 +0000

    Update context processor

--- products/views.py
+++ products/views.py
@@ -25,14 +25,14 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
 
             if sortkey == 'category':
-                sortkey = 'category__name'    
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-      
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
