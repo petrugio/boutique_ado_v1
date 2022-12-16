fdd19d927dcd3c8a81ce20a9ef39a487e3091196
petrugio <petruchelban@gmail.com>
2022-12-16 11:40:11 +0000

    Prepare app for deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 if os.path.isfile("env.py"):
     import env
     DEBUG = True
@@ -31,7 +33,7 @@ SECRET_KEY = 'django-insecure-(_co631*jhlr*pdc=c3yk8=fw0%6h)!uxspf2dm9a*#9d27q7o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['https://boutique-ado-proj.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -124,12 +126,16 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 
 
 # Password validation
